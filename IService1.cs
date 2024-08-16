c61f0f94f65fa8326c0583fca4bae2a0a3d14473
rrezeartaluzhaa <rrezeartaluzha@gmail.com>
2024-08-16 15:34:42 +0200

    wcf service

---
+++ IService1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        string ValidateEMBG(string embg); 
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;  // Ensure this namespace is included for LINQ extension methods
+using System.ServiceModel;
+
+namespace WcfService1
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string ValidateEMBG(string embg)
+        {
+            if (string.IsNullOrEmpty(embg))
+            {
+                return "EMBG cannot be empty.";
+            }
+
+            if (embg.Length != 13)
+            {
+                return "Invalid EMBG length. It should be 13 digits.";
+            }
+
+
+            if (!embg.All(char.IsDigit))
+            {
+                return "Invalid EMBG. It should contain only numeric characters.";
+            }
+
+            int[] digits = embg.Select(z => int.Parse(z.ToString())).ToArray();
+
+            int a = digits[0], b = digits[1], c = digits[2], d = digits[3];
+            int e = digits[4], f = digits[5], g = digits[6], h = digits[7];
+            int i = digits[8], j = digits[9], k = digits[10], l = digits[11];
+            int m = 11 - ((7 * (a + g) + 6 * (b + h) + 5 * (c + i) + 4 * (d + j) + 3 * (e + k) + 2 * (f + l)) % 11);
+
+            int checkDigit = (m == 10 || m == 11) ? 0 : m;
+
+            if (checkDigit == digits[12])
+            {
+                return "Valid EMBG.";
+            }
+            else
+            {
+                return "Invalid EMBG.";
+            }
+        }
+    }
+}
